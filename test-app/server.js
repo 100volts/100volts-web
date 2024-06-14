adfe8c11e7028cb12770b09cffcae8543695bcf0
irumenov <ivaylo.rumenov2@dxc.com>
2024-06-14 16:02:12 +0300

    Created routing with Expres for a single page app

--- test-app/server.js
+++ test-app/server.js
@@ -3,8 +3,10 @@ const path=require("path");
 
 const app=express();
 
+app.use("/static", express.static(path.resolve(__dirname,"frontend","static")))
+
 app.get("/*",(req,res)=>{
     res.sendFile(path.resolve("frontend","index.html"))
 })
 
-app.listen(process.env.PORT || 8081 ,()=> console.log("server running"))
+app.listen(process.env.PORT || 8082 ,()=> console.log("server running"))
