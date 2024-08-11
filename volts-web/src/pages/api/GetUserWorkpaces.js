f227c2f87e1722dbbd8b9ee3ab07a32e4aa93960
IvoRum <ivailo.rumenov@outlook.com>
2024-08-11 22:29:28 +0300

    Added getting users workspaces

---
+++ volts-web/src/pages/api/GetUserWorkpaces.js
@@ -0,0 +1,37 @@
+import pkg from "../../../package.json" assert { type: "json" };
+import Company from "../../layouts/Company.astro"
+    const address = pkg["volts-server"];
+
+    async function getUserData() {
+        try{
+            const userToken = localStorage.getItem("volts_token");
+            const response = await fetch(
+              `http://${address}:8081/api/v1/company/by/user`,
+              {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${userToken}`,
+                },
+              }
+            );
+            const datat = await response.json();
+            const { company_name } = datat;
+            document.getElementById('company_name').innerText = company_name
+            //document.getElementById('company_name').innerText = CompanyButton({companyName:company_name,companyUrl:company_name,layout:Company });
+        }catch (error) {
+            document.getElementById('result').innerText = 'Failed to fetch data: ' + error.message;
+        }
+    }
+
+    /*
+    function CompanyButton({ companyName, companyUrl, layout }) {
+        return (
+            <button onClick={layout}>
+                <a id="company_name" href={companyUrl} target="_blank">{companyName}</a>
+            </button>
+        );
+    }
+
+    */
+    getUserData();
