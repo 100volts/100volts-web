7122afb72f200d98e4cba8b77881a60518f3dc0f
IvoRum <ivailo.rumenov@outlook.com>
2024-08-13 22:54:50 +0300

    Working on react ocmponent

---
+++ volts-web/src/components/react/GetLastMeterData.jsx
@@ -0,0 +1,86 @@
+import {useState} from 'react';
+import { addElmeter, elmeters } from '../datastore/elmeterStore';
+import { useStore } from '@nanostores/react'; 
+import pkg from "../../../package.json";
+const urladdress = pkg["volts-server"];
+
+
+export default async function GetElMeterAdnDisplay(){
+    const [userElmeter, setUserElmeter]=useState('');
+    const $elmeters = useStore(elmeters)
+    await getElmeterData();
+    return(
+        <>
+        <h1>All Electric Meters</h1>
+            <label htmlFor="Electric meter">Add Electric meter</label>
+
+            <ul>
+                {
+                    $elmeters.map((elmeter,index)=><li key={index}>{elmeter}</li>)
+                }
+            </ul>
+        </>
+    )
+}
+
+async function getElmeterData() {
+    try{
+        const userToken = localStorage.getItem("volts_token");
+        const companyName = localStorage.getItem("company_name");
+        const response = await fetch(
+          `http://${urladdress}:8081/elmeter/company/address/list`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${userToken}`,
+            },
+            body: JSON.stringify({
+                company_name: companyName,
+            }),
+          },
+        );
+        const datat = await response.json();
+        const {address_list}=datat;
+        
+        address_list.forEach(async element => {
+            console.log(element)
+            await getElmeterDataFromAddress(element)
+        });
+        
+    }catch (error) {
+        console.log('Failed to fetch data: ' + error.message);
+    }
+}
+
+async function getElmeterDataFromAddress(elmeterAddress) {
+    try{
+        const userToken = localStorage.getItem("volts_token");
+        const companyName = localStorage.getItem("company_name");
+        const response = await fetch(
+          `http://${urladdress}:8081/elmeter/data/last`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${userToken}`,
+            },
+            body: JSON.stringify({
+                company_name: companyName,
+                address:elmeterAddress
+            }),
+          },
+        );
+        const datat = await response.json();
+        const {name}=datat
+        let a = document.createElement("a");
+        a.innerHTML = name;
+        document.getElementById('elmeter').appendChild(a);
+        document.getElementById('elmeter').appendChild(document.createElement("br"));
+
+        console.log(datat)
+        
+    }catch (error) {
+        console.log('Failed to fetch data: ' + error.message);
+    }
+}
