795b853d531f45fe8c0ace9b5c8c447a8a2e8b9b
irumenov <ivaylo.rumenov2@dxc.com>
2024-08-26 11:18:37 +0300

    Added displaing of the avr

--- volts-web/src/pages/api/WorplaysMeters.jsx
+++ volts-web/src/pages/api/WorplaysMeters.jsx
@@ -7,12 +7,12 @@ const ElmeterDataComponent = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const userToken = localStorage.getItem('volts_token');
+  const companyName = localStorage.getItem('company_name');
 
   // Fetch Elmeter data from the API
   const getElmeterData = async () => {
     try {
-      const userToken = localStorage.getItem('volts_token');
-      const companyName = localStorage.getItem('company_name');
       const body= JSON.stringify({
         company_name: companyName,
       })
@@ -47,8 +47,6 @@ const ElmeterDataComponent = () => {
   // Fetch individual Elmeter data by address
   const getElmeterDataFromAddress = async (elmeterAddress) => {
     try {
-      const userToken = localStorage.getItem('volts_token');
-      const companyName = localStorage.getItem('company_name');
       const response = await fetch(`http://${urladdress}:8081/elmeter/data/last`, {
         method: 'POST',
         headers: {
@@ -62,17 +60,18 @@ const ElmeterDataComponent = () => {
       });
 
       const datat = await response.json();
-      const { name, address, electric_meter_data } = datat;
+      const { name, address, electric_meter_data,electric_meter_avr_data } = datat;
 
       // Return the relevant data
       return {
         name,
         address,
         electric_meter_data,
+        electric_meter_avr_data
       };
     } catch (error) {
       console.log('Failed to fetch data: ' + error.message);
-      return null; // Return null in case of error
+      return null;
     }
   };
 
@@ -87,6 +86,7 @@ const ElmeterDataComponent = () => {
  // const chart=new Example();
   //console.log(chart)
   //<h1>Elmeter Data</h1>
+  console.log(data)
   return (
     <div>
       <br></br> 
@@ -101,12 +101,14 @@ const ElmeterDataComponent = () => {
               <td>L1</td>
               <td>L2</td>
               <td>L3</td>
+              <td>AVR</td>
             </tr>
             <tr>
               <td>Voltage:</td>
               <td>{elmeter.electric_meter_data.voltagell1}</td>
               <td>{elmeter.electric_meter_data.voltagell2}</td>
               <td>{elmeter.electric_meter_data.voltagell3}</td>
+              <td>{elmeter.electric_meter_avr_data.voltage}</td>
               <td>V</td>
             </tr>
             <tr>
@@ -114,6 +116,7 @@ const ElmeterDataComponent = () => {
               <td>{elmeter.electric_meter_data.currentl1}</td>
               <td>{elmeter.electric_meter_data.currentl2}</td>
               <td>{elmeter.electric_meter_data.currentl3}</td>
+              <td>{elmeter.electric_meter_avr_data.current}</td>
               <td>A</td>
             </tr>
             <tr>
@@ -121,6 +124,7 @@ const ElmeterDataComponent = () => {
               <td>{elmeter.electric_meter_data.activepowerl1}</td>
               <td>{elmeter.electric_meter_data.activepowerl2}</td>
               <td>{elmeter.electric_meter_data.activepowerl3}</td>
+              <td>{elmeter.electric_meter_avr_data.power}</td>
               <td>W</td>
               </tr>
             <tr>
@@ -128,6 +132,7 @@ const ElmeterDataComponent = () => {
               <td>{elmeter.electric_meter_data.pfl1}</td>
               <td>{elmeter.electric_meter_data.pfl2}</td>
               <td>{elmeter.electric_meter_data.pfl3}</td>
+              <td>{elmeter.electric_meter_avr_data.powerFactor}</td>
             </tr>
             <tr>
               <td>Total Active Power: </td>
