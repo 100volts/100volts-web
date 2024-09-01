f6118a77401ed2cb6197fddad6a92738a164d49e
IvoRum <ivailo.rumenov@outlook.com>
2024-09-02 00:46:18 +0300

    Working on form for addidng results

--- volts-web/src/pages/api/production/DisplayAllProductions.jsx
+++ volts-web/src/pages/api/production/DisplayAllProductions.jsx
@@ -1,6 +1,34 @@
-import React, { useState, useEffect } from "react";
-import pkg from "../../../../package.json";
+"use client"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import {
     Card,
     CardContent,
@@ -9,9 +37,58 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card";
-
+  import React, { useState, useEffect } from "react";
+  import pkg from "../../../../package.json";
+  
 const urladdress = pkg["volts-server"];
 
+const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  })
+  
+  
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        username: "",
+      },
+    })
+   
+    function onSubmit(values) {
+        const sendProdData = async () => {
+            try {
+              const body = JSON.stringify({
+                productionName: companyName,
+                value:values.prod_value
+              });
+              const response = await fetch(
+                `http://${urladdress}:8081/production/company/data`,
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${userToken}`,
+                  },
+                  body,
+                }
+              );
+              const datat = await response.json();
+              const { production } = datat;
+        
+              setProdData(production);
+            } catch (error) {
+              setError(error.message);
+            } finally {
+              setLoading(false);
+            }
+          };
+          useEffect(() => {
+            sendProdData();
+          }, []);
+      console.log(values)
+    }
 
 export default function DisplayAllProductions(){
     const [data, setProdData] = useState([]);
@@ -19,7 +96,7 @@ export default function DisplayAllProductions(){
     const [error, setError] = useState(null);
     const userToken = localStorage.getItem("volts_token");
     const companyName = localStorage.getItem("company_name");
-
+    const form = useForm();
     const getProdData = async () => {
         try {
           const body = JSON.stringify({
@@ -68,16 +145,46 @@ export default function DisplayAllProductions(){
                         <br></br>
                         <a>Groups:</a>
                         {production.groups.map((group, indexg) => (
-                            <div key={indexg}>
+                            <div>
                                 <a>Name: {group.name}</a>
                                 {group.electricMeters.map((electric, indexe) => (
-                                    <div key={indexe}>
+                                    <div>
                                         <a>Electric meter name: {electric.meterName}</a>
                                     </div>
                                 ))}
                             </div>
                         ))}
                     </CardContent>
+                        <Dialog>
+                    <DialogTrigger>Add new Production value</DialogTrigger>
+                    <DialogContent>
+                    <DialogHeader>
+                    <DialogTitle>Add new Production value</DialogTitle>
+                    <DialogDescription>
+                        Adding new record for the given production
+                    </DialogDescription>
+                    </DialogHeader>
+
+                    <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                        control={form.control}
+                        name="prod_value"
+                        render={({ field }) => (
+                            <FormItem>
+                            <FormLabel>Production value</FormLabel>
+                            <FormControl>
+                                <Input placeholder="123" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <Button type="submit">Submit</Button>
+                    </form>
+                    </Form>
+                    </DialogContent>
+                    </Dialog>
                 </Card>
             </div>
         ))}
