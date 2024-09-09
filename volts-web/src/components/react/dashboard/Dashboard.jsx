143949b6922ab1ea510d3a2b48d05ce34113058d
IvoRum <ivailo.rumenov@outlook.com>
2024-09-09 16:00:10 +0300

    removed dashboard for now

--- volts-web/src/components/react/dashboard/Dashboard.jsx
+++ volts-web/src/components/react/dashboard/Dashboard.jsx
@@ -66,6 +66,8 @@ export default function Charts() {
       steps: 1600,
     },
   ]
+  return(<></>);
+  
   return (
     <div className="chart-wrapper mx-auto flex max-w-6xl flex-col flex-wrap items-start justify-center gap-6 p-6 sm:flex-row sm:p-8">
       <div className="grid w-full gap-6 sm:grid-cols-2 lg:max-w-[22rem] lg:grid-cols-1 xl:max-w-[25rem]">
