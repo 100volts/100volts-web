534d5ff7da4469e9d1ee220f9ac561ef720a1a29
IvoRum <ivailo.rumenov@outlook.com>
2024-11-03 21:17:06 +0200

    Big fix of creating mater

--- volts-web/src/components/renderer/workplace/electric/CreateNewElectricMeter.jsx
+++ volts-web/src/components/renderer/workplace/electric/CreateNewElectricMeter.jsx
@@ -1,10 +1,10 @@
-"use client";
+"use client"
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
 import {
   Form,
   FormControl,
@@ -13,8 +13,8 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import {
   Dialog,
   DialogContent,
@@ -22,113 +22,123 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
+} from "@/components/ui/dialog"
 import pkg from "../../../../../package.json";
-import { userData } from "@/components/datastore/UserStore";
-import { useStore } from "@nanostores/react";
+import {userData } from "@/components/datastore/UserStore";
+import { useStore } from '@nanostores/react';
 
-export default function CreateNewElectricMeter() {
-  const formSchema = z.object({
-    electric_name: z
-      .string()
-      .min(2, {
-        message: "electric meter name must be at least 2 characters.",
-      })
-      .refine((val) => !/^\d/.test(val), {
-        message: "electric meter name cannot start with a number.",
+ 
+export default function CreateNewWaterMeter() {
+  
+const formSchema = z.object({
+    electric_name: z.string().min(2, {
+      message: "Water meter name must be at least 2 characters.",
+    })    
+    .refine((val) => !/^\d/.test(val), {
+        message: "Water meter name cannot start with a number.",
       }),
-  });
+    electric_description: z.string().min(2, {
+      message: "Water meter description must be at least 2 characters.",
+    })
+  })
 
-  const form = useForm({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      electric_name: "",
-      electric_address: "",
-    },
-  });
+  
+    const form = useForm({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        electric_name: "",
+        electric_description:""
+      },
+    })
 
-  const $userData = useStore(userData);
+  const $userData=useStore(userData);
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
     form.handleSubmit(onSubmit)(event);
-  };
+  };  
 
   async function onSubmit(values) {
-    const companyName = $userData.companies[0]; //todo remove hard coded call
-    const userToken = $userData.tokken;
+    const companyName = $userData.companies[0];//todo remove hard coded call
+    const userToken =$userData.tokken
     const urladdress = pkg["volts-server"];
-    try {
+    try{
       const body = JSON.stringify({
-        company_name: companyName,
+        company_name:companyName,
         meter_name: values.electric_name,
-        address: values.electric_address,
+        address:values.electric_description,
       });
-      const response = await fetch(`http://${urladdress}:8081/electric`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${userToken}`,
-        },
-        body,
-      });
+      const response = await fetch(
+        `http://${urladdress}:8081/elmeter`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${userToken}`,
+          },
+          body,
+        }
+      );
       const datat = await response.json();
       const { success } = datat;
-    } catch (error) {
-    } finally {
-      window.location.reload();
+    }catch (error) {}
+     finally {
+        window.location.reload();
     }
+    ;
   }
   return (
     <>
-      <Dialog>
-        <DialogTrigger>
-          <Button className="flex ">
+    <Dialog>
+    <DialogTrigger>
+        <Button className="w-full justify-start">
             <Plus className="mr-2 h-4 w-4" />
-            Create electric Meter
-          </Button>
+                Create Water Meter
+            </Button>
         </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Creating new electric Meter</DialogTitle>
-            <DialogDescription>
-              Creating a new electric Meter.
-            </DialogDescription>
-          </DialogHeader>
+    <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Creating new Water Meter</DialogTitle>
+      <DialogDescription>
+        Creating a new Water Meter.
+      </DialogDescription>
+    </DialogHeader>
 
-          <Form {...form}>
-            <form onSubmit={handleSubmit} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="electric_name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Electric meter name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="name" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="electric_address"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Electric meter Address</FormLabel>
-                    <FormControl>
-                      <Input type="number" placeholder="address" {...field} />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Create</Button>
-            </form>
-          </Form>
-        </DialogContent>
-      </Dialog>
+    <Form {...form}>
+      <form onSubmit={handleSubmit} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="electric_name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Electric meter name</FormLabel>
+              <FormControl>
+                <Input placeholder="name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="electric_description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Electric meter address</FormLabel>
+              <FormControl>
+                <Input placeholder="description" {...field} />
+              </FormControl>
+              <FormDescription>
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Create</Button>
+      </form>
+    </Form>
+    </DialogContent>
+    </Dialog>
     </>
-  );
+  )
 }
+
