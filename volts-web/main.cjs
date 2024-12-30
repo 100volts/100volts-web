505dcf59e369eb16e57ac475c2ebf7aab2059e94
IvoRum <ivailo.rumenov@outlook.com>
2024-12-30 17:45:14 +0200

    Added icon to the scripts

--- volts-web/main.cjs
+++ volts-web/main.cjs
@@ -3,18 +3,20 @@ const { app, BrowserWindow } = require('electron')
 let mainWindow
 
 app.on('ready', () => {
+  const path = require('path')
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
+    icon: path.join(__dirname, 'public/favicon.png'),
   })
 
   mainWindow.loadURL('http://localhost:8083')
 
   mainWindow.on('closed', () => {
-    mainWindow = null // Cleanup references
+    mainWindow = null 
   })
 })
 
